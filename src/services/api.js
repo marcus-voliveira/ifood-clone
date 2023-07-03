c262a3cf7428749312a1c9939175bd1b55d8ccc5
HericMD <hericmdms@gmail.com>
2023-07-03 14:47:45 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://191.52.55.223:3000',
+  baseURL: 'http://191.52.55.35:3000',
 });
+
+// porta 3000 para pc;
+// porta 19003 para 
+// sempre conferir ip
 
 export default api;
